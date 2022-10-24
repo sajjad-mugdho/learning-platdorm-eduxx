8dd0775d2f02c72dd15b7ad6ba167ce7dcf5d669
sajjad-mugdho <sajjadmugdho@yahoo.com>
2022-10-25 02:47:03 +0600

    2nd confuse commit

---
+++ src/Pages/SignUp/SignUp.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SignUp = () => {
+    return (
+        <div>
+            <h3>this is SignUp</h3>
+        </div>
+    );
+};
+
+export default SignUp;
---
+++ src/Shared/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <div className="navbar bg-base-100">
+                <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
