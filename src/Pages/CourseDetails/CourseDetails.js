9c6031e5a76928c4ab5084e13b083b7740c3b531
sajjad-mugdho <sajjadmugdho@yahoo.com>
2022-10-28 18:57:20 +0600

    download page as a pdf added

--- src/Pages/CourseDetails/CourseDetails.js
+++ src/Pages/CourseDetails/CourseDetails.js
@@ -1,12 +1,29 @@
 import React from 'react';
+import { FaFilePdf } from 'react-icons/fa';
 import { Link, useLoaderData } from 'react-router-dom';
+import html2canvas from 'html2canvas';
+import jsPDF from 'jspdf';
 
 const CourseDetails = () => {
     const courses = useLoaderData();
     const { id, name, details, img, price } = courses;
+
+    // Download page as a PDF
+    const exportPDF = () => {
+        const input = document.getElementById('htmlToPdf');
+        html2canvas(input, { logging: true, letterRendering: 1, useCORS: true }).then(canvas => {
+            const imgWidth = 208;
+            const imgHeight = canvas.height * imgWidth / canvas.width;
+            const imgData = canvas.toDataURL('img / png');
+            const pdf = new jsPDF('p', 'mm', 'a4');
+            pdf.addImage(imgData, 'PNG', 0, 0, imgWidth, imgHeight);
+            pdf.save("eduxxx.pdf")
+        })
+    }
     return (
-        <div className="card w-100 glass">
-            <figure><img src={img} alt="car!" /></figure>
+        <div id='htmlToPdf' className="card w-100 glass">
+            <button onClick={() => exportPDF()} className='btn btn-outline w-[200px] my-5 mx-auto '>DownLoad PDF <FaFilePdf></FaFilePdf></button>
+            <figure><img className='rounded-lg mt-5' src={img} alt="car!" /></figure>
             <div className="card-body">
                 <h2 className="card-title">{name}</h2>
                 <p>{details}</p>
---
+++ src/Shared/HtmlToPDF/HtmlToPDF.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HtmlToPDF = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default HtmlToPDF;
